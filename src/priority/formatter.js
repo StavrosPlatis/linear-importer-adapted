d1323e0f3973961bbd56276fb80da9ab9b801037
plat <stavrosplat@proton.me>
2025-03-24 10:22:09 +0100

    Added critical and none Priority

--- src/priority/formatter.js
+++ src/priority/formatter.js
@@ -2,12 +2,14 @@ import { detailedLogger } from "../../logger/logger_instance.js";
 
 function formatPriority(priority) {
   const pivotalPriorities = {
+    "none": 0,
+    "p0 - Critical": 1,
     "p1 - High": 2,
     "p2 - Medium": 3,
     "p3 - Low": 4,
   };
 
-  const result = pivotalPriorities[priority] || 4;
+  const result = pivotalPriorities[priority] || 0;
 
   detailedLogger.info(`formatPriority: ${priority} -> ${result}`);
 
